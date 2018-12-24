b11e73a99e5830319acd6359e578bc4e66aff568
bonhun koo <darkgs@snu.ac.kr>
2018-12-24 14:48:14 +0900

    fix MemoryError

--- src/generate_yahoo_a2v_rnn_input.py
+++ src/generate_yahoo_a2v_rnn_input.py
@@ -64,13 +64,12 @@ def generate_rnn_input(dict_url2info, dict_url2vec):
 					for _ in range(10):
 						url_triples.append((urls[i], urls[j], another_urls[random.randrange(len(another_urls))]))
 
-		url_triples = np.array(url_triples)
-		np.random.shuffle(url_triples)
+		random.shuffle(url_triples)
 		maximum_count = 20000000
 		if len(url_triples) > maximum_count:
 			url_triples = url_triples[:maximum_count]
 
-		return url_triples.tolist()
+		return url_triples
 
 	dict_rnn_input = {
 		'train': generate_triples(train_datas),
